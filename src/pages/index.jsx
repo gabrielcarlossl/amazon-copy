4caca2af924e742d28363140a4eb6c75e5c4378b
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-27 10:13:23 -0300

    favicon changed

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon.com. Spend less. Smile more.</title>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      </Head>
+      
+      <h1>Amazon</h1>
+      
+      
+    </div>
+  );
+}
