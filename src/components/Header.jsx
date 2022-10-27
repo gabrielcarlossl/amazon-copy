450210ccb31ca789939bc239caa26f9f37b76230
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-27 18:01:15 -0300

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,28 +4,38 @@ import Image from 'next/image'
 
 
 function Header() {
-  return (
-    <header className='bg-black'>
-        
-        {/* Top nav */}
-        <div>
-            <Image
-                
-                src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
-                
-                width={150}
-                height={40}
-            ></Image>
-        </div>
+    return (
+        <header className='bg-black'>
+
+            {/* Top nav */}
+            <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+
+                <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
+                    <Image
+
+                        src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                        objectFit='contain'
+                        width={150}
+                        height={40}
+                        className='cursor-pointer'
+                    ></Image>
+                </div>
+
+                {/* Search bar */}
+
+                 <div>
+                    <input type="text" name="" id="" />
+                 </div>
+            </div>
 
 
-        {/* bottom nav */}
-        <div>
+            {/* bottom nav */}
+            <div>
 
-        <h1>oi</h1>
-        </div>
-    </header>
-  )
+
+            </div>
+        </header>
+    )
 }
 
 export default Header
