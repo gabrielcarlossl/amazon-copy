be1fbed977f4f1149d02e493d625f4d0b024a140
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-28 15:03:10 -0300

    added react-carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
+
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
-        <title>Amazon.com. Spend less. Smile more.</title>
+        <title>Amazon.com.br | Tudo pra você, de A a Z.</title>
         <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
       </Head>
       
@@ -13,11 +16,13 @@ export default function Home() {
         <Header></Header>
 
         <main className="max-w-screen-2xl mx-auto">
+         
           {/* Banner */}
-          <Banner>
-            
-          </Banner>
+          <Banner></Banner>
+
           {/* Product feed */}
+          <ProductFeed></ProductFeed>
+      
         </main>
     </div>
   );
