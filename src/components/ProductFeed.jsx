350c1d2f15462896b4d33dc8abcf8fd23e8bb4a7
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-28 17:54:27 -0300

    styled the product  feed

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -3,12 +3,31 @@ import Product from './Product'
 
 function ProductFeed({ products }) {
     return (
-        <div>
-            
-            {products.map(({ id, title, price, description, category, image, rating }) => (
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+
+
+
+            {products.slice(0, 4).map(({ id, title, price, description, category, image, rating }) => (
                 <Product key={id} title={title} price={price} description={description} category={category} image={image} ></Product>
 
             ))}
+
+            <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
+
+            <div className='md:col-span-2'>
+
+                {products.slice(4, 5).map(({ id, title, price, description, category, image, rating }) => (
+                    <Product key={id} title={title} price={price} description={description} category={category} image={image} ></Product>
+
+                ))}
+
+            </div>
+
+            {products.slice(5, products.length).map(({ id, title, price, description, category, image, rating }) => (
+                <Product key={id} title={title} price={price} description={description} category={category} image={image} ></Product>
+
+            ))}
+
         </div>
     )
 }
