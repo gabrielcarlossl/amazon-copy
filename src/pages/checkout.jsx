3ea8b90bd084c5b480843015ff27ccec26201688
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-11-03 11:59:58 -0300

    added remove button and total section on chackout page

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import Header from '../components/Header'
 import Image from 'next/image'
 import { useSelector } from 'react-redux'
-import { selectItems } from '../slices/basketSlice'
+import { selectItems, selectTotal } from '../slices/basketSlice'
 import CheckoutProduct from '../components/CheckoutProduct'
+import Currency from 'react-currency-formatter'
+import { session, useSession } from 'next-auth/client'
 
 function Checkout() {
 
+    const [session] = useSession()
+
     const items = useSelector(selectItems)
+    const total = useSelector(selectTotal)
 
     return (
-        <div className='h-[100%] bg-gray-100 '>
+        <div className='h-[100%] bg-gray-100'>
             <Header></Header>
 
             <main className='lg:flex max-w-screen-2xl mx-auto'>
@@ -53,6 +58,22 @@ function Checkout() {
 
 
                 {/* right side */}
+
+                <div className='flex flex-col bg-white p-10 mx-5 my-5 shadow-md'>
+                    {items.length > 0 && (
+                        <>
+                            <h2 className='whitespace-nowrap'>Subtotal ({items.length} itens):
+                                <span className='font-bold'>
+                                {" "}<Currency quantity={total} currency='BRL'></Currency>
+                                </span>
+                            </h2>
+
+                            <button disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+                                { !session ? 'Entre para finalizar o pedido.' : 'Finalizar pedido.'}
+                            </button>
+                        </>
+                    )}
+                </div>
 
             </main>
 
